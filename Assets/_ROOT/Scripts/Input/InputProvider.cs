c09470637c0d7b607b4652717afda634897d61c8
OlesiaPashko <pashkoolesia1234@gmail.com>
2022-04-08 22:29:49 +0300

    add enabling and disabling of input

--- Assets/_ROOT/Scripts/Input/InputProvider.cs
+++ Assets/_ROOT/Scripts/Input/InputProvider.cs
@@ -1,19 +1,46 @@
 namespace Runner.Player.Input
 {
     using System;
+    using GameFlow;
     using UnityEngine;
+    using Zenject;
 
     public interface IInputProvider
     {
         event Action OnButtonClicked;
     }
     
-    public class InputProvider : MonoBehaviour, IInputProvider
+    public class InputProvider : MonoBehaviour, IInputProvider, IInitializable
     {
+        [Inject]
+        public SignalBus SignalBus { get; set; }
         public event Action OnButtonClicked;
+
+        private bool isActive;
+        
+        public void Initialize()
+        {
+            SignalBus.Subscribe<LevelFailed>(DisableInput);
+            SignalBus.Subscribe<LevelStarted>(EnableInput);
+        }
+
+        private void EnableInput()
+        {
+            isActive = true;
+        }
+        
+        private void DisableInput()
+        {
+            isActive = false;
+        }
 
         private void Update()
         {
+            if (!isActive)
+            {
+                return;
+            }
+            
             if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
             {
                 OnButtonClicked?.Invoke();
